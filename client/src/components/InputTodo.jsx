59ebafec4294809c12f1208162130ed07f005eda
Hersh Thakur <hersh.thakur@gmail.com>
2023-08-24 10:31:49 +0100

    connected client and server side. All created components to ppost new tasks as well as get all tasks that have been created

---
+++ client/src/components/InputTodo.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+export default function InputTodo() {
+	const [description, setDescription] = useState("");
+
+	const onSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			const body = { description };
+			const res = await fetch("http://localhost:4000/todos", {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify(body),
+			});
+			console.log(res);
+			window.location = "/";
+		} catch (err) {
+			console.error(err);
+		}
+	};
+
+	return (
+		<div className=" w-full flex flex-col items-center">
+			<h1 className=" mt-5 text-3xl">PERN Todo List</h1>
+			<form className=" flex mt-5" onSubmit={onSubmit}>
+				<input
+					type="text"
+					placeholder="Add New Task"
+					value={description}
+					onChange={(e) => setDescription(e.target.value)}
+					className="  border-grey-300 border focus:border-indigo-600 p-1"
+				/>
+				<button className=" rounded-tr rounded-br bg-emerald-400 pl-3 pr-3 text-white">
+					Add
+				</button>
+			</form>
+		</div>
+	);
+}
---
+++ client/src/components/ListTodos.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+
+export default function ListTodos() {
+	const [todos, setTodos] = useState([]);
+
+	const getTodos = async () => {
+		try {
+			const res = await fetch("http://localhost:4000/todos");
+			const jsonData = await res.json();
+			setTodos(jsonData);
+		} catch (err) {
+			console.error(err);
+		}
+	};
+
+	useEffect(() => {
+		getTodos();
+	}, []);
+
+	return (
+		<div>
+			<h1 className=" text-center mt-5 text-2xl">All Todo Items</h1>
+			<div className=" flex justify-center">
+				<table className="mt-5">
+					<tr className=" border-t border-b border-gray-300">
+						<th className="p-3">Description</th>
+						<th className="p-3">Edit</th>
+						<th className="p-3">Delete</th>
+					</tr>
+					{todos.map((item) => (
+						<tr key={item} className=" border-b">
+							<td className="p-3">{item.description}</td>
+							<td className="p-3">
+								<button className="border rounded-lg pr-2 pl-2 bg-blue-500 text-white">
+									Edit
+								</button>
+							</td>
+							<td className="p-3">
+								<button className="border rounded-lg pr-2 pl-2 bg-red-500 text-white">
+									Delete
+								</button>
+							</td>
+						</tr>
+					))}
+				</table>
+			</div>
+		</div>
+	);
+}
